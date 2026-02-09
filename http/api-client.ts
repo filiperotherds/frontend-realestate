86c729b7bc0371e254f17f9371222c26c5354e19
Filipe Rother de Souza <filiperother@icloud.com>
2026-02-09 13:01:22 -0300

    add api client

---
+++ http/api-client.ts
@@ -0,0 +1,28 @@
+import ky from "ky";
+import { getCookie } from "cookies-next";
+
+export const api = ky.create({
+  prefixUrl: process.env.API_URL,
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let token;
+
+        if (typeof window === "undefined") {
+          const { cookies: serverCookies } = await import("next/headers");
+
+          const cookieStore = await serverCookies();
+
+          token = cookieStore.get("token")?.value;
+        } else {
+          token = getCookie("token");
+        }
+
+        if (token) {
+          request.headers.set("Authorization", `Bearer ${token}`);
+        }
+      },
+    ],
+  },
+  cache: "no-store",
+});
